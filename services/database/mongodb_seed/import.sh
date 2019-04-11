8547f06ae4d9b1624b7c3e102bd31269b8eeb602
dingding <ruixue.fi@gmail.com>
2019-04-12 00:52:46 +0300

    wait script in import.sh added

--- services/database/mongodb_seed/import.sh
+++ services/database/mongodb_seed/import.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-chmod +x /wait
-/wait 
+# While until connecting to mongodb succeeds
+# refer to: https://docs.docker.com/compose/startup-order/
+while [ -n "$(mongo --host mkongodb -u root -p root --authenticationDatabase admin --eval "quit()" |grep "Connection refused")" ]; 
+do
+  sleep 1
+done
+mongo --host mongodb -u root -p root --authenticationDatabase admin --eval "quit()" |grep "Connection refused"
 mongoimport --host mongodb -u root -p root --authenticationDatabase admin --db masterdata --collection cities --file cities.json
 mongoimport --host mongodb -u root -p root --authenticationDatabase admin --db masterdata --collection shops --file shops.json
 mongo --host mongodb -u root -p root --authenticationDatabase admin < auth.js
