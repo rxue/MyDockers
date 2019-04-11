8edab717efc93aa0662fc8f3f0d1f87e419020f5
dingding <ruixue.fi@gmail.com>
2019-04-11 20:34:36 +0300

    wait program installed on mongodb seed

--- services/database/mongodb_seed/import.sh
+++ services/database/mongodb_seed/import.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mongoimport --host mongodb -u root -p root --authenticationDatabase admin --db masterdata --collection cities --file cities.json
+/wait && mongoimport --host mongodb -u root -p root --authenticationDatabase admin --db masterdata --collection cities --file cities.json
 mongoimport --host mongodb -u root -p root --authenticationDatabase admin --db masterdata --collection shops --file shops.json
 mongo --host mongodb -u root -p root --authenticationDatabase admin < auth.js
