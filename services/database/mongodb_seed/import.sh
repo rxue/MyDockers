123cf4a9445fa811ccc920205112888e6bc1d802
dingding <ruixue.fi@gmail.com>
2019-04-11 20:44:21 +0300

    docker for Dockerfile and import.sh refactored

--- services/database/mongodb_seed/import.sh
+++ services/database/mongodb_seed/import.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-/wait && mongoimport --host mongodb -u root -p root --authenticationDatabase admin --db masterdata --collection cities --file cities.json
+chmod +x /wait
+/wait 
+mongoimport --host mongodb -u root -p root --authenticationDatabase admin --db masterdata --collection cities --file cities.json
 mongoimport --host mongodb -u root -p root --authenticationDatabase admin --db masterdata --collection shops --file shops.json
 mongo --host mongodb -u root -p root --authenticationDatabase admin < auth.js
