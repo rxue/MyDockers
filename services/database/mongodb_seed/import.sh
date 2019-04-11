c2cdb4900c3982271e3a306ae137dff1282485ce
dingding <ruixue.fi@gmail.com>
2019-04-12 00:55:45 +0300

    a typo removed

--- services/database/mongodb_seed/import.sh
+++ services/database/mongodb_seed/import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # While until connecting to mongodb succeeds
 # refer to: https://docs.docker.com/compose/startup-order/
-while [ -n "$(mongo --host mkongodb -u root -p root --authenticationDatabase admin --eval "quit()" |grep "Connection refused")" ]; 
+while [ -n "$(mongo --host mongodb -u root -p root --authenticationDatabase admin --eval "quit()" |grep "Connection refused")" ]; 
 do
   sleep 1
 done
