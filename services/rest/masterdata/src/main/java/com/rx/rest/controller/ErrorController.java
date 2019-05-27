52f33fb63eea2aacfcf8d00f44ae915095e29bde
dingding <ruixue.fi@gmail.com>
2019-05-27 22:02:05 +0300

    web.xml

---
+++ services/rest/masterdata/src/main/java/com/rx/rest/controller/ErrorController.java
@@ -0,0 +1,23 @@
+package com.rx.rest.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import com.rx.rest.error.ErrorAttributes;
+import com.rx.rest.error.ErrorAttributesBuilder;
+
+@RestController
+public class ErrorController {
+		
+		@RequestMapping(value = "/errors")
+	    public ErrorAttributes renderError(HttpServletRequest httpRequest, WebRequest webRequest) {
+			Integer statusCode = (Integer) httpRequest.getAttribute("javax.servlet.error.status_code");
+			System.out.println("DEBUG:: status code is " + statusCode);
+			return new ErrorAttributesBuilder(webRequest, true).build();
+	    }
+		
+		
+}
