d239ff8b0365a5979eb6ec90739f044e032507d1
dingding <ruixue.fi@gmail.com>
2019-05-28 23:43:29 +0300

    with a minimal web.xml, ErrorAttributes can display properly now

--- services/rest/masterdata/src/main/java/com/rx/rest/aop/RestExceptionHandler.java
+++ services/rest/masterdata/src/main/java/com/rx/rest/aop/RestExceptionHandler.java
@@ -1,27 +1,43 @@
 package com.rx.rest.aop;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.util.WebUtils;
 import com.rx.rest.error.ErrorAttributesBuilder;
 
+/**
+ * @author dingding
+ *
+ */
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {		request.setAttribute(WebUtils.ERROR_STATUS_CODE_ATTRIBUTE, status.value(), RequestAttributes.SCOPE_REQUEST);
-		String[] attrs = request.getAttributeNames(RequestAttributes.SCOPE_REQUEST);
-		for (String a : attrs)
-			System.out.println("DEBUG::" + a + " = " + request.getAttribute(a, RequestAttributes.SCOPE_REQUEST));
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {		
+		request.setAttribute(WebUtils.ERROR_STATUS_CODE_ATTRIBUTE, status.value(), RequestAttributes.SCOPE_REQUEST);
 		request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, RequestAttributes.SCOPE_REQUEST);
+		request.setAttribute(RequestDispatcher.ERROR_REQUEST_URI, getPath(request), RequestAttributes.SCOPE_REQUEST);
 		ErrorAttributesBuilder errorAttributesBuilder = new ErrorAttributesBuilder(request, true, false);
+		
 		//BindingResult re = ((MethodArgumentNotValidException) ex).getBindingResult();
 		return new ResponseEntity<>(errorAttributesBuilder.build(), headers, status);
+	}
+	
+	private String getPath(WebRequest servletWebRequest) {
+		if (servletWebRequest instanceof ServletWebRequest) {
+			HttpServletRequest request = ((ServletWebRequest) servletWebRequest).getRequest();
+			return request.getRequestURI(); 
+		}
+		return null;
 	}
 }
