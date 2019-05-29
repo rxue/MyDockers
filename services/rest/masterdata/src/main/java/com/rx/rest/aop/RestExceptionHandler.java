ad55b6d10c7956e64935b9418a5e59dd49992c13
dingding <ruixue.fi@gmail.com>
2019-05-29 22:26:40 +0300

    in better condition now in branch web_xml_config

--- services/rest/masterdata/src/main/java/com/rx/rest/aop/RestExceptionHandler.java
+++ services/rest/masterdata/src/main/java/com/rx/rest/aop/RestExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -31,17 +30,6 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 		ErrorAttributesBuilder errorAttributesBuilder = new ErrorAttributesBuilder(request, true, false);
 		return new ResponseEntity<>(errorAttributesBuilder.build(), headers, status);
 	}
-
-//	@Override
-//	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {		
-//		request.setAttribute(WebUtils.ERROR_STATUS_CODE_ATTRIBUTE, status.value(), RequestAttributes.SCOPE_REQUEST);
-//		request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, RequestAttributes.SCOPE_REQUEST);
-//		request.setAttribute(RequestDispatcher.ERROR_REQUEST_URI, getPath(request), RequestAttributes.SCOPE_REQUEST);
-//		ErrorAttributesBuilder errorAttributesBuilder = new ErrorAttributesBuilder(request, true, false);
-//		
-//		//BindingResult re = ((MethodArgumentNotValidException) ex).getBindingResult();
-//		return new ResponseEntity<>(errorAttributesBuilder.build(), headers, status);
-//	}
 	
 	private String getPath(WebRequest servletWebRequest) {
 		if (servletWebRequest instanceof ServletWebRequest) {
--- services/rest/masterdata/src/main/java/com/rx/rest/config/WebConfiguration.java
+++ services/rest/masterdata/src/main/java/com/rx/rest/config/WebConfiguration.java
@@ -1,22 +1,14 @@
 package com.rx.rest.config;
 
-import java.util.Enumeration;
 import java.util.List;
-import java.util.Optional;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.lang.Nullable;
 
 @Configuration
 @EnableWebMvc
