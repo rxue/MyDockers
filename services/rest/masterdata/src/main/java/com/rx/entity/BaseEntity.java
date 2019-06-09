c1507c8a6dd48fa7953c5f4b2a99e9af41f7b39a
dingding <ruixue.fi@gmail.com>
2019-06-09 04:00:57 +0300

    Lombok used for removing the setters and getters bolilerplate code from entity classes

--- services/rest/masterdata/src/main/java/com/rx/entity/BaseEntity.java
+++ services/rest/masterdata/src/main/java/com/rx/entity/BaseEntity.java
@@ -2,17 +2,10 @@ package com.rx.entity;
 
 import org.springframework.data.annotation.Id;
 
+import lombok.Data;
+
+@Data
 public abstract class BaseEntity {
 	@Id
 	private String id;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-
 }
--- services/rest/masterdata/src/main/java/com/rx/entity/City.java
+++ services/rest/masterdata/src/main/java/com/rx/entity/City.java
@@ -3,22 +3,12 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+@Data
 @Document(collection="cities")
 public class City extends BaseEntity {
 	private String country;
 	@NotNull(message="city cannot be empty")
 	private String city;
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
 	
 }
